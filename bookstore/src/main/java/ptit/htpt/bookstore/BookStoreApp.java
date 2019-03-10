c860b27efe714c7b718b5ad157b13bcda91465bd
toantm <tuti133@gmail.com>
2019-03-10 20:55:59 +0700

    first commit, create spring boot app

---
+++ bookstore/src/main/java/ptit/htpt/bookstore/BookStoreApp.java
@@ -0,0 +1,13 @@
+package ptit.htpt.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreApp.class, args);
+	}
+
+}
